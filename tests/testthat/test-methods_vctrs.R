dc69f36d156e92f8fa793a4cfc1f47ec01d6813d
mariusbarth <marius.barth@uni-koeln.de>
2020-10-18 21:07:59 +0200

    updated tests

--- tests/testthat/test-methods_vctrs.R
+++ tests/testthat/test-methods_vctrs.R
@@ -7,16 +7,24 @@ test_that(
     variable_label(obj2) <- "b"
 
     # Both labelled:
-    expect_identical(vec_ptype2(obj1, obj2), integer(0))
+    expect_warning(expect_identical(vec_ptype2(obj1, obj2), integer(0)))
 
-    expect_identical(vec_ptype2(obj1, TRUE), integer(0))
-    expect_identical(vec_ptype2(obj1, 1L), integer(0))
-    expect_identical(vec_ptype2(obj1, 1.4), double(0))
-    expect_identical(vec_ptype2(obj1, complex(1)), complex(0))
+    labelled_integer <- integer(0L)
+    labelled_double  <- double(0L)
+    labelled_complex <- complex(0L)
+    variable_label(labelled_integer) <- "a"
+    variable_label(labelled_double)  <- "a"
+    variable_label(labelled_complex) <- "a"
 
-    expect_identical(vec_ptype2(logical(1), obj1), integer(0))
-    expect_identical(vec_ptype2(integer(1), obj1), integer(0))
-    expect_identical(vec_ptype2(double(1) , obj1), double(0))
-    expect_identical(vec_ptype2(complex(1), obj1), complex(0))
+    expect_identical(vec_ptype2(obj1, TRUE), labelled_integer)
+    expect_identical(vec_ptype2(TRUE, obj1), labelled_integer)
+    expect_identical(vec_ptype2(obj1, 1L), labelled_integer)
+    expect_identical(vec_ptype2(obj1, 1.4), labelled_double)
+    expect_identical(vec_ptype2(obj1, complex(1)), labelled_complex)
+
+    expect_identical(vec_ptype2(logical(1), obj1), labelled_integer)
+    expect_identical(vec_ptype2(integer(1), obj1), labelled_integer)
+    expect_identical(vec_ptype2(double(1) , obj1), labelled_double)
+    expect_identical(vec_ptype2(complex(1), obj1), labelled_complex)
   }
 )
